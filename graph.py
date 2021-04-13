b211bf9d35a103a7154117c519cdcc8ef40723a9
Zayd Tahir <zaydtahir20@gmail.com>
2021-04-13 13:22:22 -0400

    update graph.py

--- graph.py
+++ graph.py
@@ -9,9 +9,9 @@ fig, axs = plt.subplots(2, 2)
 
 for pos in range(0, 3):
 
-    a1 = 0.9414141414141415 * 100
-    a2 = 2.475757575757576 * 100
-    a3 = 2.07979797979798 * 100
+    a1 = 0.9303030303030303 * 100
+    a2 = 2.4843434343434345 * 100
+    a3 = 2.1 * 100
 
     xdist = math.cos(theta[pos]) * a3
     ydist = math.sin(theta[pos]) * a3
@@ -19,25 +19,22 @@ for pos in range(0, 3):
     Ex = g_x[pos] - xdist
     Ey = g_y[pos] - ydist
 
-    q2 = -math.acos((math.pow(Ex, 2) + math.pow(Ey, 2) - math.pow(a1, 2) - math.pow(a2, 2)) / (2 * a1 * a2));
-    q1 = math.atan(Ey / Ex) + math.atan((a2 * math.sin(q2)) / (a1 + a2 * math.cos(q2)))
+    q2_p = math.acos((math.pow(Ex, 2) + math.pow(Ey, 2) - math.pow(a1, 2) - math.pow(a2, 2)) / (2 * a1 * a2));
+    q1_p = math.atan(Ey / Ex) + math.atan((a2 * math.sin(q2_p)) / (math.sqrt(math.pow(Ex, 2) + math.pow(Ey, 2))))
 
-    a1_x = math.cos(q1) * a1
-    a1_y = math.sin(q1) * a1
-
-    a2_x = math.cos(q1 - q2) * a2 + a1_x
-    a2_y = math.sin(q1 - q2) * a2 + a1_y
+    a1_x_p = math.cos(q1_p) * a1
+    a1_y_p = math.sin(q1_p) * a1
 
     if pos == 0:
-        axs[0, 0].plot([0, a1_x], [0, a1_y], marker = '.')
-        axs[0, 0].plot([a1_x, a2_x], [a1_y, a2_y], marker = '.')
-        axs[0, 0].plot([a2_x, g_x[pos]], [a2_y, g_y[pos]], marker = '.')
+        axs[0, 0].plot([0, a1_x_p], [0, a1_y_p], marker = '.')
+        axs[0, 0].plot([a1_x_p, Ex], [a1_y_p, Ey], marker = '.')
+        axs[0, 0].plot([Ex, g_x[pos]], [Ey, g_y[pos]], marker = '.')
     elif pos == 1:
-        axs[0, 1].plot([0, a1_x], [0, a1_y], marker = '.')
-        axs[0, 1].plot([a1_x, a2_x], [a1_y, a2_y], marker = '.')
-        axs[0, 1].plot([a2_x, g_x[pos]], [a2_y, g_y[pos]], marker = '.')
+        axs[0, 1].plot([0, a1_x_p], [0, a1_y_p], marker = '.')
+        axs[0, 1].plot([a1_x_p, Ex], [a1_y_p, Ey], marker = '.')
+        axs[0, 1].plot([Ex, g_x[pos]], [Ey, g_y[pos]], marker = '.')
     else:
-        axs[1, 0].plot([0, a1_x], [0, a1_y], marker = '.')
-        axs[1, 0].plot([a1_x, a2_x], [a1_y, a2_y], marker = '.')
-        axs[1, 0].plot([a2_x, g_x[pos]], [a2_y, g_y[pos]], marker = '.')
+        axs[1, 0].plot([0, a1_x_p], [0, a1_y_p], marker = '.')
+        axs[1, 0].plot([a1_x_p, Ex], [a1_y_p, Ey], marker = '.')
+        axs[1, 0].plot([Ex, g_x[pos]], [Ey, g_y[pos]], marker = '.')
 plt.show()
