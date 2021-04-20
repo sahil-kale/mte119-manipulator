104b522bafad2d19b747ab80521fa695b1d37ebf
Sahil Kale <32375512+sahil-kale@users.noreply.github.com>
2021-04-20 12:22:03 -0600

    Threaded (#1)
    * Threaded Stuff and Working Visualiser
    
    * Add in iterator
    
    * torque math
    
    * update graph.py
    
    * fixing something
    
    * Update main.py
    
    * pushing
    
    Co-authored-by: Zayd Tahir <zaydtahir20@gmail.com>
    Co-authored-by: Aaditya <bluebird1339@gmail.com>

--- graph.py
+++ graph.py
@@ -1,34 +1,40 @@
 import matplotlib.pyplot as plt
-from math import *
+import math
 
-if __name__ == '__main__':
-    g_x = [0.75, 0.50, 0.20]
-    g_y = [0.10, 0.50, 0.60]
-    theta = [-0.60 / 180 * pi, 0, 0.45 / 180 * pi]
+g_x = [75, 50, 20]
+g_y = [10, 50, 60]
+theta = [-60 * math.pi / 180, 0, 45 * math.pi / 180]
 
-    pos = 2
+fig, axs = plt.subplots(2, 2)
 
-    a1 = 0.7102204408817635
-    a2 = 1.6923847695390781
-    a3 = 1.5064128256513027
+for pos in range(0, 3):
 
-    xdist = cos(theta[pos]) * a3
-    ydist = sin(theta[pos]) * a3
+    a1 = 0.9303030303030303 * 100
+    a2 = 2.4843434343434345 * 100
+    a3 = 2.1 * 100
+
+    xdist = math.cos(theta[pos]) * a3
+    ydist = math.sin(theta[pos]) * a3
 
     Ex = g_x[pos] - xdist
     Ey = g_y[pos] - ydist
 
-    q2 = -acos((Ex * Ex + Ey * Ey - a1 * a1 - a2 * a2) / (2 * a1 * a2))
-    q1 = atan(Ey / Ex) + atan((a2 * sin(q2)) / (a1 + a2 * cos(q2)))
+    q2_p = math.acos((math.pow(Ex, 2) + math.pow(Ey, 2) - math.pow(a1, 2) - math.pow(a2, 2)) / (2 * a1 * a2));
+    q1_p = math.atan(Ey / Ex) + math.atan((a2 * math.sin(q2_p)) / (a1 + a2 * math.cos(q2_p)))
 
-    a1_x = cos(q1) * a1
-    a1_y = sin(q1) * a1
+    a1_x_p = math.cos(q1_p) * a1
+    a1_y_p = math.sin(q1_p) * a1
 
-    a2_x = cos(q1 - q2) * a2 + a1_x
-    a2_y = sin(q1 - q2) * a2 + a1_y
-
-    x_val = [0, a1_x, a2_x, g_x[pos]]
-    y_val = [0, a1_y, a2_y, g_y[pos]]
-
-    plt.plot(x_val, y_val, marker = '.')
-    plt.show()
+    if pos == 0:
+        axs[0, 0].plot([0, a1_x_p], [0, a1_y_p], marker = '.')
+        axs[0, 0].plot([a1_x_p, Ex], [a1_y_p, Ey], marker = '.')
+        axs[0, 0].plot([Ex, g_x[pos]], [Ey, g_y[pos]], marker = '.')
+    elif pos == 1:
+        axs[0, 1].plot([0, a1_x_p], [0, a1_y_p], marker = '.')
+        axs[0, 1].plot([a1_x_p, Ex], [a1_y_p, Ey], marker = '.')
+        axs[0, 1].plot([Ex, g_x[pos]], [Ey, g_y[pos]], marker = '.')
+    else:
+        axs[1, 0].plot([0, a1_x_p], [0, a1_y_p], marker = '.')
+        axs[1, 0].plot([a1_x_p, Ex], [a1_y_p, Ey], marker = '.')
+        axs[1, 0].plot([Ex, g_x[pos]], [Ey, g_y[pos]], marker = '.')
+plt.show()
